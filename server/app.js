9286e8f2d53ebe7bee58641654d108f557e2be13
krishan2005op <krishanmalhotra2005@gmail.com>
2024-06-25 12:10:13 +0530

    updated

--- server/app.js
+++ server/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const {readFileSync} = require('fs')
 
 //get all files
-const homePage = readFileSync('./index.html')
+const homePage = readFileSync('./node-express-course/02-express-tutorial/navbar-app/index.html')
 
 const server = http.createServer((req,res)=>{
     const url = req.url;
